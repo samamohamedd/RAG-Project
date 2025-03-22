73c206b406210213e77ca34477e6b343137a6eaa
Sama mohamed mahmoud abd ellatif <143009290+samamohamedd@users.noreply.github.com>
2025-03-22 02:17:00 +0200

    return file id

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -21,7 +21,7 @@ class DataController(BaseController):
         
         return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
     
-    def generate_unique_filename(self, original_file_name: str, project_id: str):
+    def generate_unique_filepath(self, original_file_name: str, project_id: str):
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
 
@@ -38,7 +38,7 @@ class DataController(BaseController):
             random_key + "_" + cleaned_filename
         )
             
-        return new_file_path
+        return new_file_path, random_key + "_" + cleaned_filename
     
 
     def get_clean_filename(self, original_file_name: str):
--- src/routes/data.py
+++ src/routes/data.py
@@ -31,7 +31,7 @@ async def upload_data(project_id: str,
             )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_filename(
+    file_path, file_id = data_controller.generate_unique_filepath(
         original_file_name=file.filename,
         project_id=project_id
         )
@@ -51,7 +51,8 @@ async def upload_data(project_id: str,
 
     return JSONResponse(
         content={
-            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id": file_id
         }
     )
 
